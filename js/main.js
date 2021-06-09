3b0903546438ecea2c3643d117b34aec0eba616d
Anaena <carrie@mail.ru>
2021-06-09 19:33:53 +0300

    Добавляет изменение имен переменных и функций

--- js/main.js
+++ js/main.js
@@ -5,20 +5,20 @@ const SIMILAR_DESCRIPTION_NUMBER = 25;
 const MIN_LIKES = 15;
 const MAX_LIKES = 200;
 
-const similarComment = new Array(SIMILAR_DESCRIPTION_NUMBER).fill(null).map(createComment);
+const similarComments = new Array(SIMILAR_DESCRIPTION_NUMBER).fill(null).map(createComment);
 
-const similarPhotoDescription = () => {
-  const createPhotoDescription = [];
+const createPhoto = () => {
+  const descriptions = [];
   for (let id = 1; id <= SIMILAR_DESCRIPTION_NUMBER; id++) {
-    createPhotoDescription.push({
+    descriptions.push({
       id: id,
       url: `photos/${id}.jpg`,
       description: DESCRIPTION[getRandomNumber(0, DESCRIPTION.length - 1)],
       likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
-      comments: similarComment[getRandomNumber(0, similarComment.length - 1)],
+      comments: similarComments[getRandomNumber(0, similarComments.length - 1)],
     });
   }
-  return createPhotoDescription;
+  return descriptions;
 };
 
-similarPhotoDescription();
+createPhoto();
