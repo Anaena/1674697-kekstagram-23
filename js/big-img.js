29829df53ca111163f04df3684404c3f9189740f
Anaena <carrie@mail.ru>
2021-06-29 12:51:56 +0300

    Исправляет счетчик комментариев

--- js/big-img.js
+++ js/big-img.js
@@ -13,6 +13,13 @@ const socialCommentCount = bigPicture.querySelector('.social__comment-count');
 const commentsLoaderButton = bigPicture.querySelector('.comments-loader');
 const pageBody = document.querySelector('body');
 
+const closePictureModal = () => {
+  bigPicture.classList.add('hidden');
+  pageBody.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', onPopupEscKeydown);
+};
+
 const renderComment = ({ avatar, name, message }) => {
   const commentBlock = document.createElement('li');
   const commentPicture = document.createElement('img');
@@ -32,44 +39,45 @@ const renderComment = ({ avatar, name, message }) => {
   commentsList.appendChild(commentBlock);
 };
 
+const renderComments = (comments) => comments.forEach(renderComment);
+const renderMoreComments = (comments) => {
+  const displayedCommentsCount = commentsList.querySelectorAll('.social__comment').length;
+  renderComments(comments.slice(displayedCommentsCount, displayedCommentsCount + COMMENTS_STEP));
+  const numberComments = commentsList.querySelectorAll('.social__comment').length;
+  if (numberComments === comments.length) {
+    commentsLoaderButton.classList.add('hidden');
+    socialCommentCount.classList.add('hidden');
+  } else {
+    socialCommentCount.classList.remove('hidden');
+    commentsLoaderButton.classList.remove('hidden');
+  }
+  socialCommentCount.innerHTML = `${numberComments} из <span class="comments-count">${comments.length}</span> комментариев`;
+};
+
 const openPictureModal = (picture) => {
   bigPictureImg.src = picture.url;
   likesCount.textContent = picture.likes;
   commentsCount.textContent = picture.comments.length;
   pictureDescription.textContent = picture.description;
   commentsList.innerHTML = '';
-  const renderComments = (comments) => comments.forEach(renderComment);
-  const renderMoreComments = (comments) => {
-    const displayedCommentsCount = commentsList.querySelectorAll('.social__comment').length;
-    renderComments(comments.slice(displayedCommentsCount, displayedCommentsCount + COMMENTS_STEP));
-    const numberComments = commentsList.querySelectorAll('.social__comment').length;
-    if (numberComments === comments.length) {
-      commentsLoaderButton.classList.add('hidden');
-      socialCommentCount.classList.add('hidden');
-    } else {
-      socialCommentCount.classList.remove('hidden');
-      commentsLoaderButton.classList.remove('hidden');
-    }
-    socialCommentCount.innerHTML = `${numberComments} из <span class="comments-count">${comments.length}</span> комментариев`;
-  };
   renderMoreComments(picture.comments);
   commentsLoaderButton.addEventListener('click', () => {
     renderMoreComments(picture.comments);
+  });
+  pictureCloseElement.addEventListener('click', () => {
+    closePictureModal();
+    commentsList.innerHTML = '';
+    picture.comments = '';
   });
   bigPicture.classList.remove('hidden');
   pageBody.classList.add('modal-open');
   document.addEventListener('keydown', onPopupEscKeydown);
 };
 
-const closePictureModal = () => {
-  bigPicture.classList.add('hidden');
-  pageBody.classList.remove('modal-open');
-
-  document.removeEventListener('keydown', onPopupEscKeydown);
-};
-
-pictureCloseElement.addEventListener('click', () => {
-  closePictureModal();
-});
+// pictureCloseElement.addEventListener('click', (comments) => {
+//   closePictureModal();
+//   commentsList.innerHTML = '';
+//   // comments = '';
+// });
 
 export { pictureCloseElement, openPictureModal, closePictureModal, pageBody };
